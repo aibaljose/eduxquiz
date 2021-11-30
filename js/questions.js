2c1d679405b07804d2dc44190c1a548ccac28d93
Aibal jose <69980574+aibaljose@users.noreply.github.com>
2021-11-30 19:45:49 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,85 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: " The Receipts and payments account is a_______.",
+    answer: "Real Account",
+    options: [
+      "Impersonal Account",
+      "Personal Account",
+      "Nominal Account",
+      "Real Account"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Choose odd one !.",
+    answer: "Tsunami trading company",
+    options: [
+      "Tsunami arts and club",
+      "Tsunami trading company",
+      "Tsunami artis club",
+      "Tsunami library"
+    ]
+  },
+    {
+    numb: 3,
+    question: " ______represents the excess of the assets over liabilities.",
+    answer: " Capital Fund",
+    options: [
+      " Capital Fund",
+      "Assets",
+      "Revenue",
+      "Liabilities"
+    ]
+  },
+    {
+    numb: 4,
+    question: " income and Expenditure A/c is prepared in order to ascertain_______.?",
+    answer: "Surplus or deficit.",
+    options: [
+      "Profit or Loss",
+      "Assets and Liabilities",
+      "Surplus or deficit.",
+      "Cash in hand and at bank"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The Receipts and payment account contains______. ?",
+    answer: "All receipts and payments",
+    options: [
+      "Capital receipts and payments only",
+      "Revenue receipts and payments only",
+      "All receipts and payments",
+      "Petty receipts and payments only"
+    ]
+    
+  },
+  //{
+ // numb: 6,
+ // question: "What does XML stand for?",
+ // answer: "eXtensible Markup Language",
+ // options: [
+ //   "eXtensible Markup Language",
+  //  "eXecutable Multiple Language",
+  //  "eXTra Multi-Program Language",
+  //  "eXamine Multiple Language"
+ // ]  
+
+ // },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+   //  {
+    // numb: 7,
+   //  question: "Your Question is Here",
+  //  answer: "Correct answer of the question is here",
+   //  options: [
+   //    "Option 1",
+    //   "option 2",
+    //  "option 3",
+    //   "option 4"
+   //  ]
+   //},
+];
